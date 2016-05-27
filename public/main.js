07be5c0cd68dde9d5da15acaa9b5eac2f2ac7ec7
jon-wade <jonwadeuk@gmail.com>
2016-05-27 11:28:56 +0100

    add in drawing on mousedown functionality

---
+++ public/main.js
@@ -0,0 +1,38 @@
+var pictionary = function() {
+    var canvas, context;
+    var drawing = false;
+
+    var draw = function(position) {
+        context.beginPath();
+        context.arc(position.x, position.y,
+                         6, 0, 2 * Math.PI);
+        context.fill();
+    };
+
+    canvas = $('canvas');
+    context = canvas[0].getContext('2d');
+    canvas[0].width = canvas[0].offsetWidth;
+    canvas[0].height = canvas[0].offsetHeight;
+    canvas.on('mousedown', function() {
+        drawing = true;
+        return drawing;
+    });
+    canvas.on('mouseup', function() {
+        drawing = false;
+        return drawing;
+    });
+
+
+    canvas.on('mousemove', function(event) {
+        if (drawing) {
+            var offset = canvas.offset();
+            var position = {x: event.pageX - offset.left,
+                y: event.pageY - offset.top};
+            draw(position);
+        }
+    });
+};
+
+$(document).ready(function() {
+    pictionary();
+});
